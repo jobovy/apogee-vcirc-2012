9716ff72d3c032ddcc3a4c33d43b449a6f0d7d3e
Jo Bovy <jo.bovy@gmail.com>
2011-04-21 00:15:10 +0000

    vclos: first tests of sampling

--- py/plot_fake_data.py
+++ py/plot_fake_data.py
@@ -6,14 +6,16 @@ import math as m
 import numpy as nu
 from optparse import OptionParser
 #import subprocess
-from galpy.orbit import Orbit
+#from galpy.orbit import Orbit
 from galpy.df import dehnendf, shudf
-def create_fake_los(parser):
+from galpy.util import bovy_plot
+_DEGTORAD= m.pi/180.
+def plot_fake_data(parser):
     """
     NAME:
-       create_fake_los
+       plot_fake_data
     PURPOSE:
-       create a fake APOGEE LOS
+       plot the fake data created by create_fake_data
     INPUT:
        parser - from optparse
     OUTPUT:
@@ -21,51 +23,53 @@ def create_fake_los(parser):
     HISTORY:
        2011-04-20 - Written - Bovy (NYU)
     """
-    nu.random.seed(seed=1)
     (options,args)= parser.parse_args()
     if len(args) == 0:
         parser.print_help()
         sys.exit(-1)
     #Set up DF
-    print "Setting up DF ..."
     dfc= dehnendf(beta=options.beta,profileParams=(options.rd,options.rs,options.so),correct=True,niter=20)
-    #Sample until we have nobjects
-    print "Sampling the LOS ..."
-    n= 0
-    out= []
-    while n < options.nobjects:
-        thisos= dfc.sampleLOS(los=options.los,n=options.nobjects-n)
-        #add distance uncertainty
-        thisout= []
-        for o in thisos:
-            basico= Orbit([o.R(),o.vR(),o.vT(),0.,0.,o.phi()])#HACK bc ll does not work in 2D currently
-            thisd= basico.dist(obs=[1.,0.,0.],ro=1.)
-            #Add uncertainty logarithmically, like photometric distance uncertainty TO DO
-            thisd+= nu.random.normal()*thisd*options.distuncertainty
-            thisout.append(Orbit([basico.ll(obs=[1.,0.,0.],ro=1.)[0],
-                                  thisd[0],
-                                  basico.pmll(obs=[1.,0.,0.,0.,1.,0.],
-                                              ro=1.,vo=1.)[0],
-                                  basico.vlos(obs=[1.,0.,0.,0.,1.,0.],
-                                              ro=1.,vo=1.)[0]],
-                                 lb=True,
-                                 solarmotion=[0.,0.,0.],ro=1.,vo=1.))
-        ds= nu.array([o.dist(obs=[1.,0.,0.],ro=1.) for o in thisout]).flatten()
-        thisos= [thisout[ii] for ii in range(len(thisout)) \
-                     if (ds[ii] < options.dmax)]
-        out.extend(thisos)
-        n+= len(thisos)
-    if len(out) > options.nobjects:
-        out= out[0:options.nobjects-1]
-    #Save
-    picklefile= open(args[0],'wb')
-    pickle.dump(out,picklefile)
+    #Open pickle
+    picklefile= open(args[0],'rb')
+    out= pickle.load(picklefile)
     picklefile.close()
-    return None                   
+    if not options.vlosdname is None:
+        #Plot distribution of vlos vs. d
+        vloss=nu.array([o.vlos(obs=[1.,0.,0.,0.,1.,0.],ro=1.,vo=1.) \
+                            for o in out]).flatten()
+        ds= nu.array([o.dist(obs=[1.,0.,0.],ro=1.) \
+                          for o in out]).flatten()
+        #Also calculate the expected relation
+        ntheory= 1001
+        dx= nu.linspace(0.,2.,ntheory)
+        vtheory= nu.zeros(ntheory)
+        l= options.los*_DEGTORAD
+        for ii in range(ntheory):
+            R= nu.sqrt(1.+dx[ii]**2.-2.*dx[ii]*nu.cos(l))
+            if 1./nu.cos(l) < dx[ii] and nu.cos(l) > 0.:
+                phi= nu.pi-m.asin(dx[ii]/R*nu.sin(l))
+            else:
+                phi= m.asin(dx[ii]/R*nu.sin(l))
+            vtheory[ii]= (R**options.beta-dfc.asymmetricdrift(R))*m.sin(phi+l)-m.sin(l) #last term is the LSR
+        bovy_plot.bovy_print()
+        bovy_plot.scatterplot(ds,vloss,'.',bins=options.scatterbins,
+                              xlabel=r'$d / R_0$',
+                              ylabel=r'$v_{los} / v_0$')
+        bovy_plot.bovy_plot(dx,vtheory,overplot=True)
+        bovy_plot.bovy_text(r'$l = %i^\circ$' % round(options.los),
+                            top_left=True)
+        bovy_plot.bovy_end_print(options.vlosdname)
 
 def get_options():
-    usage = "usage: %prog [options] <savefilename>\n\nsavefilename= name of the file that the los will be saved to (as a pickle)"
+    usage = "usage: %prog [options] <savefilename>\n\nsavefilename= name of the file that holds the los (as a pickle)"
     parser = OptionParser(usage=usage)
+    #Same options as create_fake_data and then some
+    parser.add_option("--vlosdname",dest="vlosdname",
+                      default=None,
+                      help="Plot of the distribution of v_los and d")
+    parser.add_option("--scatterbins",dest="scatterbins",type='int',
+                      default=15,
+                      help="Number of bins to use in scatterplot (in one dimension)")
     parser.add_option("-n","--nobjects",dest="nobjects",type='int',
                       default=500,
                       help="Number of objects to simulate")
@@ -96,4 +100,5 @@ def get_options():
     return parser
 
 if __name__ == '__main__':
-    create_fake_los(get_options())
+    plot_fake_data(get_options())
+
