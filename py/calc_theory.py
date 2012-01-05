8a45f0404782efae01915379335cb078ada65b65
Jo Bovy <jo.bovy@gmail.com>
2012-01-05 05:06:48 +0000

    vclos: restart files

--- py/calc_theory.py
+++ py/calc_theory.py
@@ -1,10 +1,15 @@
 #Calculate some theoretical stuff
+import sys
 import os, os.path
 import cPickle as pickle
 import math
 import numpy
 from galpy.df import dehnendf, shudf
 from galpy.util import bovy_coords, save_pickles
+_ERASESTR= "                                                                                "
+#ignore annoying warnings
+import warnings
+warnings.filterwarnings("ignore")
 #Overall setup
 nls= 501
 ls= numpy.linspace(0.,360.,nls)
@@ -25,8 +30,17 @@ def safe_dl_to_rphi(d,l):
         theta= math.asin(d/R*math.sin(l))
     return (R,theta,d,l)
 def calc_pred(pred_file,dfc,nls,nds,ls):
-    avg_pred= numpy.zeros(nls)
-    for ii in range(nls):
+    if os.path.exists(pred_file):
+        savefile= open(pred_file,'rb')
+        ls= pickle.load(savefile)
+        avg_pred= pickle.load(savefile)
+        ii= pickle.load(savefile)
+    else:
+        avg_pred= numpy.zeros(nls)
+        ii= 0
+    while ii < len(ls):
+        sys.stdout.write('\r'+"Working on %i / %i ...\r" %(ii+1,len(ls)))
+        sys.stdout.flush()
         meanvlos= 0.
         norm= 0.
         for jj in range(nds):
@@ -36,55 +50,52 @@ def calc_pred(pred_file,dfc,nls,nds,ls):
             meanvlos+= surfmass*dfc.meanvT(R)*math.sin(theta+l)
             norm+= surfmass
         avg_pred[ii]= meanvlos/norm
-        save_pickles(pred_file,ls,avg_pred)
+        ii+= 1
+        save_pickles(pred_file,ls,avg_pred,ii)
+    sys.stdout.write('\r'+_ERASESTR+'\r')
+    sys.stdout.flush()
     save_pickles(pred_file,ls,avg_pred)
 
 #Start calculating
 #Fiducial
 pred_file= os.path.join(_SAVEDIR,'l_vhelio_fid.sav')
-if not os.path.exists(pred_file):
-    print "Working on fiducial ..."
-    dfc= dehnendf(beta=0.,correct=True,niter=20)
-    calc_pred(pred_file,dfc,nls,nds,ls)
+print "Working on fiducial ..."
+dfc= dehnendf(beta=0.,correct=True,niter=20)
+calc_pred(pred_file,dfc,nls,nds,ls)
 #betas
 betas= [-0.2,-0.1,0.1,0.2]
 for beta in betas:
     pred_file= os.path.join(_SAVEDIR,'l_vhelio_beta_%.2f.sav' % beta)
-    if not os.path.exists(pred_file):
-        print "Working on beta %.2f ..." % beta
-        dfc= dehnendf(beta=beta,correct=True,niter=20)
-        calc_pred(pred_file,dfc,nls,nds,ls)
+    print "Working on beta %.2f ..." % beta
+    dfc= dehnendf(beta=beta,correct=True,niter=20)
+    calc_pred(pred_file,dfc,nls,nds,ls)
 #hr
 hrs= [1./4.,1./2.]
 for hr in hrs:
     pred_file= os.path.join(_SAVEDIR,'l_vhelio_hr_%.6f.sav' % hr)
-    if not os.path.exists(pred_file):
-        print "Working on hr %.2f ..." % hr
-        dfc= dehnendf(beta=0.,correct=True,niter=20,
-                      profileParams=(hr,1.,0.2))
-        calc_pred(pred_file,dfc,nls,nds,ls)
+    print "Working on hr %.2f ..." % hr
+    dfc= dehnendf(beta=0.,correct=True,niter=20,
+                  profileParams=(hr,1.,0.2))
+    calc_pred(pred_file,dfc,nls,nds,ls)
 #hs
 hs= 2./3.
 pred_file= os.path.join(_SAVEDIR,'l_vhelio_hs_%.6f.sav' % hs)
-if not os.path.exists(pred_file):
-    print "Working on hs %.2f ..." % hs
-    dfc= dehnendf(beta=0.,correct=True,niter=20,
-                  profileParams=(1./3.,hs,0.2))
-    calc_pred(pred_file,dfc,nls,nds,ls)
+print "Working on hs %.2f ..." % hs
+dfc= dehnendf(beta=0.,correct=True,niter=20,
+              profileParams=(1./3.,hs,0.2))
+calc_pred(pred_file,dfc,nls,nds,ls)
 #shu
 pred_file= os.path.join(_SAVEDIR,'l_vhelio_shu.sav')
-if not os.path.exists(pred_file):
-    print "Working on Shu ..."
-    dfc= shudf(beta=0.,correct=True,niter=20)
-    calc_pred(pred_file,dfc,nls,nds,ls)
+print "Working on Shu ..."
+dfc= shudf(beta=0.,correct=True,niter=20)
+calc_pred(pred_file,dfc,nls,nds,ls)
 altdmaxs= [5./8.,15./8.]
 for altdmax in altdmaxs:
     thisds= numpy.linspace(0.,altdmax,nds)
     pred_file= os.path.join(_SAVEDIR,'l_vhelio_dmax_%.6f.sav' % altdmax)
-    if not os.path.exists(pred_file):
-        print "Working on dmax %.2f ..." % altdmax
-        dfc= dehnendf(beta=0.,correct=True,niter=20)
-        calc_pred(pred_file,dfc,nls,thisnds,ls)
+    print "Working on dmax %.2f ..." % altdmax
+    dfc= dehnendf(beta=0.,correct=True,niter=20)
+    calc_pred(pred_file,dfc,nls,thisnds,ls)
 
 #sig0.1
 sig= 0.1
