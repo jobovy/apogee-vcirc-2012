adb1472f2ec7442c20c0e6e6016c9b3db09877c5
Jo Bovy <jo.bovy@gmail.com>
2012-01-17 16:11:34 +0000

    vclos: more sigv calculations

--- py/calc_theory.py
+++ py/calc_theory.py
@@ -131,11 +131,10 @@ calc_pred(pred_file,dfc,nls,nds,ls,ds)
 #sig0.1
 sig= 0.1
 pred_file= os.path.join(_SAVEDIR,'l_vhelio_sig_%.6f.sav' % sig)
-if not os.path.exists(pred_file):
-    print "Working on sig %.2f ..." % sig
-    dfc= dehnendf(beta=0.,correct=True,niter=20,
-                  profileParams=(1./3.,1.,sig))
-    calc_pred(pred_file,dfc,nls,nds,ls,ds)
+print "Working on sig %.2f ..." % sig
+dfc= dehnendf(beta=0.,correct=True,niter=20,
+              profileParams=(1./3.,1.,sig))
+calc_pred(pred_file,dfc,nls,nds,ls,ds)
 #hs
 hs= 2./3.
 pred_file= os.path.join(_SAVEDIR,'l_vhelio_hs_%.6f.sav' % hs)
@@ -151,3 +150,52 @@ avg_file= os.path.join(_SAVEDIR,'l_vhelio_fid.sav')
 print "Working on fiducial ..."
 dfc= dehnendf(beta=0.,correct=True,niter=20)
 calc_pred_sigv(pred_file,avg_file,dfc,nls,nds,ls,ds)
+#betas
+betas= [-0.2,-0.1,0.1,0.2]
+for beta in betas:
+    avg_file= os.path.join(_SAVEDIR,'l_vhelio_beta_%.2f.sav' % beta)
+    pred_file= os.path.join(_SAVEDIR,'l_vhelio_sigv_beta_%.2f.sav' % beta)
+    print "Working on beta %.2f ..." % beta
+    dfc= dehnendf(beta=beta,correct=True,niter=20)
+    calc_pred_sigv(pred_file,avg_file,dfc,nls,nds,ls,ds)
+#hr
+hrs= [1./4.,1./2.]
+for hr in hrs:
+    avg_file= os.path.join(_SAVEDIR,'l_vhelio_hr_%.6f.sav' % hr)
+    pred_file= os.path.join(_SAVEDIR,'l_vhelio_sigv_hr_%.6f.sav' % hr)
+    print "Working on hr %.2f ..." % hr
+    dfc= dehnendf(beta=0.,correct=True,niter=20,
+                  profileParams=(hr,1.,0.2))
+    calc_pred_sigv(pred_file,avg_file,dfc,nls,nds,ls,ds)
+#Alt dmax
+altdmaxs= [5./8.,15./8.]
+for altdmax in altdmaxs:
+    thisds= numpy.linspace(0.,altdmax,nds)
+    avg_file= os.path.join(_SAVEDIR,'l_vhelio_dmax_%.6f.sav' % altdmax)
+    pred_file= os.path.join(_SAVEDIR,'l_vhelio_sigv_dmax_%.6f.sav' % altdmax)
+    print "Working on dmax %.2f ..." % altdmax
+    dfc= dehnendf(beta=0.,correct=True,niter=20)
+    calc_pred_sigv(pred_file,avg_file,dfc,nls,nds,ls,thisds)
+#shu
+avg_file= os.path.join(_SAVEDIR,'l_vhelio_shu.sav')
+pred_file= os.path.join(_SAVEDIR,'l_vhelio_sigv_shu.sav')
+print "Working on Shu ..."
+dfc= shudf(beta=0.,correct=True,niter=20)
+calc_pred_sigv(pred_file,avg_file,dfc,nls,nds,ls,ds)
+#sig0.1
+sig= 0.1
+pred_file= os.path.join(_SAVEDIR,'l_vhelio_sigv_sig_%.6f.sav' % sig)
+avg_file= os.path.join(_SAVEDIR,'l_vhelio_sig_%.6f.sav' % sig)
+print "Working on sig %.2f ..." % sig
+dfc= dehnendf(beta=0.,correct=True,niter=20,
+              profileParams=(1./3.,1.,sig))
+calc_pred_sigv(pred_file,avg_file,dfc,nls,nds,ls,ds)
+#hs
+hs= 2./3.
+avg_file= os.path.join(_SAVEDIR,'l_vhelio_sigv_hs_%.6f.sav' % hs)
+pred_file= os.path.join(_SAVEDIR,'l_vhelio_hs_%.6f.sav' % hs)
+print "Working on hs %.2f ..." % hs
+dfc= dehnendf(beta=0.,correct=True,niter=20,
+              profileParams=(1./3.,hs,0.2))
+calc_pred_sigv(pred_file,avg_file,dfc,nls,nds,ls,ds)
+
