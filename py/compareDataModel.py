b175cb1ecea1e2d3189685991a1ea30557fa79f0
Jo Bovy <jo.bovy@gmail.com>
2012-02-21 00:10:46 +0000

    vclos: start on forward model, add dwarf and outlier models

---
+++ py/compareDataModel.py
@@ -0,0 +1,177 @@
+###################################################################################
+#  compareDataModel.py: module for comparing the data to the model
+###################################################################################
+from optparse import OptionParser
+import numpy
+from scipy.maxentropy import logsumexp
+from galpy.util import bovy_plot
+import multi
+import multiprocessing
+from fitvc import mloglike, _DEGTORAD, _REFV0, _REFR0
+from readVclosData import readVclosData
+import isomodel
+def pvlosplate(params,vhelio,data,iso,df,options):
+    """
+    NAME:
+       pvlosplate
+    PURPOSE:
+       calculate the vlos probability for a given location
+    INPUT:
+       params - parameters of the model
+       vhelio - heliocentric los velocity to evaluate
+       data - data array for this location
+       iso - isochrone model
+       df - df object(s) (?)
+       options - options
+    OUTPUT:
+       log of the probability
+    HISTORY:
+       2012-02-20 - Written - Bovy (IAS)
+    """
+    #Output is sum over data l,b,jk,h
+    l= data['GLON']*_DEGTORAD
+    b= data['GLAT']*_DEGTORAD
+    sinl= numpy.sin(l)
+    cosl= numpy.cos(l)
+    sinb= numpy.sin(b)
+    cosb= numpy.cos(b)
+    jk= data['J0MAG']-data['K0MAG']
+    jk[(jk < 0.5)]= 0.5 #BOVY: FIX THIS HACK BY EMAILING GAIL
+    h= data['H0MAG']
+    out= numpy.zeros(len(data))
+    for ii in range(len(out)):
+        out[ii]= -mloglike(params,numpy.array([vhelio]),
+                           numpy.array([l[ii]]),
+                           numpy.array([b[ii]]),
+                           numpy.array([jk[ii]]),
+                           numpy.array([h[ii]]),
+                           iso,df,options,
+                           numpy.array([sinl[ii]]),
+                           numpy.array([cosl[ii]]),
+                           numpy.array([cosb[ii]]),
+                           numpy.array([sinb[ii]]))
+    return logsumexp(out)
+
+def get_options():
+    usage = "usage: %prog [options] <savefilename>\n\nsavefilename= name of the file that the fit/samples will be saved to"
+    parser = OptionParser(usage=usage)
+    #Rotation curve parameters/model
+    parser.add_option("--rotcurve",dest='rotcurve',default='flat',
+                      help="Rotation curve model to fit")
+    #Velocity distribution model
+    parser.add_option("--dfmodel",dest='dfmodel',default='simplegaussian',
+                      help="DF model to use")
+    #Density model
+    parser.add_option("--densmodel",dest='densmodel',default='expdisk',
+                      help="Density model to use")
+    parser.add_option("--hr",dest='hr',default=3.,type='float',
+                      help="scale length in kpc")
+    parser.add_option("--hz",dest='hz',default=0.3,type='float',
+                      help="scale height in kpc")
+    parser.add_option("--hs",dest='hs',default=8.,type='float',
+                      help="dispersion scale length in kpc")
+    #Data options
+    parser.add_option("--lmin",dest='lmin',default=35.,type='float',
+                      help="readVclosData 'lmin'")
+    parser.add_option("--bmax",dest='bmax',default=2.,type='float',
+                      help="readVclosData 'bmax'")
+    parser.add_option("--allshutdown",action="store_false", 
+                      dest="postshutdown",
+                      default=True,
+                      help="setting this sets postshutdown to False in data")
+    parser.add_option("--fehcut",action="store_true", 
+                      dest="fehcut",
+                      default=False,
+                      help="readVclosData 'fehcut'")
+    parser.add_option("--cohort",dest='cohort',default=None,
+                      help="readVclosData 'cohort'")
+    parser.add_option("--jkmax",dest='jkmax',default=1.2,type='float',
+                      help="readVclosData 'jkmax'")
+    parser.add_option("--location",dest='location',default=4318,type='int',
+                      help="location id when looking at single los")
+    #Isochrone IMF
+    parser.add_option("--imfmodel",dest='imfmodel',default='lognormalChabrier2001',
+                      help="imfmodel for isochrone model")
+    parser.add_option("--Z",dest='Z',default=.019,type='float',
+                      help="Metallicity of isochrone")
+    #Add dwarf part?
+    parser.add_option("--dwarf",action="store_true", 
+                      dest="dwarf",
+                      default=False,
+                      help="setting this adds dwarf contamination")
+    #Distance marginalization by binning or not
+    parser.add_option("--dontbintegrate",action="store_true",
+                      dest="dontbintegrate",
+                      default=False,
+                      help="If set, *don't* calculate the integral by binning in d")
+    #Comparison options
+    parser.add_option("--plottype",dest='plottype',default='pvloslos',
+                       help="Type of plot to make")
+    parser.add_option("-o","--plotfile",dest='plotfile',
+                      help="name of file for plot")
+    parser.add_option("--nvlos",dest='nvlos',default=11,type='int',
+                      help="number of vlos")
+    #Multiprocessing?
+    parser.add_option("-m","--multi",dest='multi',default=None,type='int',
+                      help="number of cpus to use")
+    return parser
+
+if __name__ == '__main__':
+    #Get options
+    parser= get_options()
+    options,args= parser.parse_args()
+    #Read data
+    data= readVclosData(postshutdown=options.postshutdown,
+                        fehcut=options.fehcut,
+                        cohort=options.cohort,
+                        lmin=options.lmin,
+                        bmax=options.bmax,
+                        ak=True,
+                        jkmax=options.jkmax)
+    #Set up the isochrone
+    print "Setting up the isochrone model ..."
+    iso= isomodel.isomodel(imfmodel=options.imfmodel,Z=options.Z)
+    if options.dwarf:
+        iso= [iso, 
+              isomodel.isomodel(imfmodel=options.imfmodel,Z=options.Z,dwarf=True)]
+    df= None
+    #test
+    if options.plottype.lower() == 'pvloslos':
+        print set(list(data['LOCATION']))
+        data= data[(data['LOCATION'] == options.location)]
+        #Fit parameters
+        if options.dwarf:
+            params= [270./_REFV0,8./_REFR0,numpy.log(35./_REFV0),0.025,0.5,1.]
+        else:
+            params= [270./_REFV0,8./_REFR0,numpy.log(35./_REFV0),0.025,0.5]
+        #Calculate vlos los
+        vlos= numpy.linspace(-200.,200.,options.nvlos)
+        pvlos= numpy.zeros(options.nvlos)
+        if not options.multi is None:
+            pvlos= multi.parallel_map((lambda x: pvlosplate(params,vlos[x],
+                                                            data,iso,df,options)),
+                                      range(options.nvlos),
+                                      numcores=numpy.amin([len(vlos),multiprocessing.cpu_count(),options.multi]))
+        else:
+            for ii in range(options.nvlos):
+                print ii
+                pvlos[ii]= pvlosplate(params,vlos[ii],data,iso,df,options)
+        pvlos-= logsumexp(pvlos)
+        pvlos= numpy.exp(pvlos)
+        #Plot data
+        bovy_plot.bovy_print()
+        hist, xvec, p= bovy_plot.bovy_hist(data['VHELIO'],range=[-200.,200.],
+                                           bins=31,
+                                           histtype='step',color='k',
+                                           xlabel=r'$\mathrm{heliocentric}\ v_{\mathrm{los}}\ [\mathrm{km\ s}^{-1}]$')
+        #Normalize prediction
+        data_int= numpy.sum(hist)*(xvec[1]-xvec[0])
+        pvlos*= data_int/numpy.sum(pvlos)/(vlos[1]-vlos[0])
+        bovy_plot.bovy_plot(vlos,pvlos,'-',color='0.65',overplot=True)
+        #Add text
+        bovy_plot.bovy_text(r'$\mathrm{location}\ =\ %i$' % options.location
+                            +'\n'
+                            +r'$l\ \approx\ %.0f^\circ$' % numpy.mean(data['GLON']),
+                            top_right=True,size=14.)
+        bovy_plot.bovy_end_print(options.plotfile)
+
--- py/fitvc.py
+++ py/fitvc.py
@@ -10,6 +10,11 @@
 #        - Outlier model
 #        - Correct Ro prior
 #
+# BaseModel: Flat rotation curve, Ro, 
+#            Gaussian velocity distribution + outlier model (frac + mean, 
+#            fixed 100 km/s dispersion)
+#            --dwarf adds same for dwarfs
+#
 ###############################################################################
 import sys
 import os, os.path
@@ -32,6 +37,8 @@ _REFR0= 8. #kpc
 _BINTEGRATENBINS= 1001
 _BINTEGRATEDMIN= 0.001 #kpc
 _BINTEGRATEDMAX= 30. #kpc
+_BINTEGRATEDMIN_DWARF= 0.001 #kpc
+_BINTEGRATEDMAX_DWARF= 1. #kpc
 _DEGTORAD= math.pi/180.
 _ERASESTR= "                                                                                "
 def fitvc(parser):
@@ -66,6 +73,11 @@ def fitvc(parser):
     #Set up the isochrone
     print "Setting up the isochrone model ..."
     iso= isomodel.isomodel(imfmodel=options.imfmodel,Z=options.Z)
+    if options.dwarf:
+        iso= [iso,
+              isomodel.isomodel(imfmodel=options.imfmodel,Z=options.Z,dwarf=True)]
+    else:
+        iso= [iso]
     df= None
     #Initial condition for fit/sample
     init_params= _initialize_params(options)
@@ -83,7 +95,10 @@ def fitvc(parser):
 
 def _initialize_params(options):
     if options.rotcurve.lower() == 'flat' and options.dfmodel.lower() == 'simplegaussian':
-        return [235./_REFV0,8./_REFR0,numpy.log(35./_REFV0)]
+        if options.dwarf:
+            return [235./_REFV0,8./_REFR0,numpy.log(35./_REFV0),0.1,0.,0.1]
+        else:
+            return [235./_REFV0,8./_REFR0,numpy.log(35./_REFV0),0.1,0.]
 
 def cb(x): print x
 
@@ -95,19 +110,33 @@ def mloglike(params,vhelio,l,b,jk,h,iso,df,options,sinl,cosl,cosb,sinb):
         return numpy.finfo(numpy.dtype(numpy.float64)).max
     elif params[1] <= 5./_REFR0 or params[1] > 11./_REFR0: #So it does not go nuts
         return numpy.finfo(numpy.dtype(numpy.float64)).max
+    if params[3] < 0. or params[3] > 1.:
+        return numpy.finfo(numpy.dtype(numpy.float64)).max
+    if options.dwarf and (params[5] < 0. or params[5] > 1.):
+        return numpy.finfo(numpy.dtype(numpy.float64)).max
     #For each star, marginalize over distance
     if options.dontbintegrate:
         out= 0.
         for ii in range(len(vhelio)):
             sys.stdout.write('\r'+"Working on %i / %i ...\r" % (ii,len(vhelio)))
             sys.stdout.flush()
-            thisout= integrate.quad(_mloglikedIntegrand,
-                                    0.,numpy.inf,
-                                    args=(params,vhelio[ii]/params[0]/_REFV0,
-                                          l[ii],b[ii],jk[ii],h[ii],
-                                          iso,df,options,
-                                          sinl[ii],cosl[ii],cosb[ii],sinb[ii],
-                                          False))[0]
+            raise NotImplementedError("Direct scipy.integrate integration does not work bc of returning the p(d)")
+            thisout, thispd= integrate.quad(_mloglikedIntegrand,
+                                            0.,numpy.inf,
+                                            args=(params,vhelio[ii]/params[0]/_REFV0,
+                                                  l[ii],b[ii],jk[ii],h[ii],
+                                                  iso[0],df,options,
+                                                  sinl[ii],cosl[ii],cosb[ii],sinb[ii],
+                                                  False))[0]
+            if options.dwarf:
+                thisout= (1.-params[5])*thisout\
+                    +params[5]*integrate.quad(_mloglikedIntegrand,
+                                              0.,numpy.inf,
+                                              args=(params,vhelio[ii]/params[0]/_REFV0,
+                                                    l[ii],b[ii],jk[ii],h[ii],
+                                                    iso[1],df,options,
+                                                    sinl[ii],cosl[ii],cosb[ii],sinb[ii],
+                                                    False))[0]
             if thisout == 0.:
                 print vhelio[ii], l[ii]/_DEGTORAD, jk[ii], h[ii]
             out+= -numpy.log(thisout)
@@ -116,18 +145,39 @@ def mloglike(params,vhelio,l,b,jk,h,iso,df,options,sinl,cosl,cosb,sinb):
         print out, params
     else: #Calculate the integral by binning
         thisout= numpy.zeros((len(vhelio),_BINTEGRATENBINS))
+        logpd= numpy.zeros((len(vhelio),_BINTEGRATENBINS))
         ds= numpy.linspace(_BINTEGRATEDMIN,_BINTEGRATEDMAX,
                            _BINTEGRATENBINS)/params[1]/_REFR0
+        dd= ds[1]-ds[0]
+        if options.dwarf:
+            thisxtraout= numpy.zeros((len(vhelio),_BINTEGRATENBINS))
+            logpddwarf= numpy.zeros((len(vhelio),_BINTEGRATENBINS))
+            if options.dwarf:
+                dwarfds= numpy.linspace(_BINTEGRATEDMIN_DWARF,_BINTEGRATEDMAX_DWARF,
+                                        _BINTEGRATENBINS)/params[1]/_REFR0
+                dwarfdd= dwarfds[1]-dwarfds[0]
         for ii in range(_BINTEGRATENBINS):
-            thisout[:,ii]= _mloglikedIntegrand(ds[ii],
-                                               params,vhelio/params[0]/_REFV0,
-                                               l,b,jk,h,iso,df,options,
-                                               sinl,cosl,cosb,sinb,
-                                               True)
+            thisout[:,ii], logpd[:,ii]= _mloglikedIntegrand(ds[ii],
+                                                            params,vhelio/params[0]/_REFV0,
+                                                            l,b,jk,h,iso[0],df,options,
+                                                            sinl,cosl,cosb,sinb,
+                                                            True)+numpy.log(dd)
+            if options.dwarf:
+                thisxtraout[:,ii], logpddwarf[:,ii]= _mloglikedIntegrand(dwarfds[ii],
+                                                                         params,vhelio/params[0]/_REFV0,
+                                                                         l,b,jk,h,iso[1],df,options,
+                                                                         sinl,cosl,cosb,sinb,
+                                                                         True)+numpy.log(dwarfdd)
         #Sum each one
         out= 0.
         for ii in range(len(vhelio)):
-            out+= -logsumexp(thisout[ii,:])
+            if options.dwarf: #Combine
+                tmpthisout= logsumexp(thisout[ii,:])+numpy.log(1.-params[5])-logsumexp(logpd[ii,:])
+                tmpthisxtraout= logsumexp(thisxtraout[ii,:])+numpy.log(params[5])-logsumexp(logpddwarf[ii,:])
+                c= numpy.amax([tmpthisout,tmpthisxtraout])
+                out-= numpy.log(numpy.exp(tmpthisout-c)+numpy.exp(tmpthisxtraout-c))+c
+            else:
+                out+= -logsumexp(thisout[ii,:])
         print out, params
     #BOVY:Apply Ro prior correcly
     return out+(params[1]*_REFR0-8.2)**2./0.5#+(params[0]*_REFV0+2.25*math.exp(2.*params[2])*_REFV0/params[0]+12.24-_PMSGRA*params[1]*_REFR0)**2./200. #params[1]=Ro, SBD10 Solar motion
@@ -136,6 +186,8 @@ def _mloglikedIntegrand(d,params,vhelio,l,b,jk,h,
                         iso,df,options,sinl,cosl,cosb,sinb,returnlog):
     #All positions are /ro, all velocities are /vo (d and vhelio have this already
     #Calculate coordinates, distances are /Ro (/params[1])
+    #
+    #Returns the loglike and the p(d) factor
     R= numpy.sqrt(1.+d**2.-2.*d*cosl)
     if isinstance(vhelio,numpy.ndarray):
         indx= (R == 0.)
@@ -158,8 +210,8 @@ def _mloglikedIntegrand(d,params,vhelio,l,b,jk,h,
     #Calculate probabilities
     logpvlos= _logdf(params,vpec,R,options,df,l,theta)
     logpd= _logpd(params,d,l,b,jk,h,df,iso,options,R,theta,cosb,sinb)
-    if returnlog: return logpvlos+logpd
-    else: return numpy.exp(logpvlos+logpd)
+    if returnlog: return (logpvlos+logpd,logpd)
+    else: return (numpy.exp(logpvlos+logpd),numpy.exp(logpd))
 
 def _logpd(params,d,l,b,jk,h,df,iso,options,R,theta,cosb,sinb):
     dm= _dm(params,d)
@@ -183,10 +235,10 @@ def _logdf(params,vpec,R,options,df,l,theta):
     if options.dfmodel.lower() == 'simplegaussian':
         sinlt= numpy.sin(l+theta)
         slos= numpy.exp(params[2])/params[0]\
-            *numpy.sqrt(1.-0.5*sinlt**2.)
+            *numpy.sqrt(1.-0.5*sinlt**2.)*numpy.exp(-(R-1.)/options.hs*params[1]*_REFR0)
         t= vpec/slos
-        #return numpy.log(0.95*norm.pdf(t)/slos/params[0]+0.05*norm.pdf(vpec*params[0]*_REFV0/200.)/200.)
-        return norm.logpdf(t)-numpy.log(slos*params[0])
+        return numpy.log((1.-params[3])*norm.pdf(t)/slos/params[0]/_REFV0+params[3]*norm.pdf((vpec*params[0]-params[4])*_REFV0/100.)/100.)
+        #return norm.logpdf(t)-numpy.log(slos*params[0])
 
 def _vc(params,R,options):
     """Circular velocity at R for different models"""
@@ -215,6 +267,8 @@ def get_options():
                       help="scale length in kpc")
     parser.add_option("--hz",dest='hz',default=0.3,type='float',
                       help="scale height in kpc")
+    parser.add_option("--hs",dest='hs',default=8.,type='float',
+                      help="dispersion scale length in kpc")
     #Data options
     parser.add_option("--lmin",dest='lmin',default=35.,type='float',
                       help="readVclosData 'lmin'")
@@ -237,6 +291,11 @@ def get_options():
                       help="imfmodel for isochrone model")
     parser.add_option("--Z",dest='Z',default=.019,type='float',
                       help="Metallicity of isochrone")
+    #Add dwarf part?
+    parser.add_option("--dwarf",action="store_true", 
+                      dest="dwarf",
+                      default=False,
+                      help="setting this adds dwarf contamination")
     #Distance marginalization by binning or not
     parser.add_option("--dontbintegrate",action="store_true",
                       dest="dontbintegrate",
