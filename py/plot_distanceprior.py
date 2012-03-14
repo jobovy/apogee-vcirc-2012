89a3afe89019038762ed46430572a8fa82ff713c
Jo Bovy <jo.bovy@gmail.com>
2012-03-14 22:49:42 +0000

    vclos: distance prior plot

--- py/plot_distanceprior.py
+++ py/plot_distanceprior.py
@@ -43,10 +43,10 @@ def plot_distanceprior(parser):
     iso= isomodel.isomodel(imfmodel=options.imfmodel,Z=options.Z,
                            expsfh=options.expsfh)
     #Set up polar grid
-    res= 31
+    res= 51
     xgrid= numpy.linspace(0.,2.*math.pi*(1.-1./res/2.),
                        2*res)
-    ygrid= numpy.linspace(0.5,2.2,res)
+    ygrid= numpy.linspace(0.5,2.8,res)
     plotxgrid= numpy.linspace(xgrid[0]-(xgrid[1]-xgrid[0])/2.,
                               xgrid[-1]+(xgrid[1]-xgrid[0])/2.,
                               len(xgrid)+1)
@@ -54,7 +54,7 @@ def plot_distanceprior(parser):
                               ygrid[-1]+(ygrid[1]-ygrid[0])/2.,
                               len(ygrid)+1)
     plotthis= numpy.zeros((2*res,res,len(data)))-numpy.finfo(numpy.dtype(numpy.float64)).max
-    #_BINTEGRATENBINS= 31
+    #_BINTEGRATENBINS= 11 #For quick testing
     ds= numpy.linspace(_BINTEGRATEDMIN,_BINTEGRATEDMAX,_BINTEGRATENBINS)
     logpiso= numpy.zeros((len(data),_BINTEGRATENBINS))
     dm= _dm(ds)
@@ -69,19 +69,21 @@ def plot_distanceprior(parser):
         theta= numpy.arcsin(d/R*sinl)
         indx= (1./cosl < d)*(cosl > 0.)
         theta[indx]= numpy.pi-theta[indx]
+        indx= (theta < 0.)
+        theta[indx]+= 2.*math.pi
         thisout= _logpd([0.,1.],d,None,None,
                         None,None,None,
                         options,R,theta,
                         1.,0.,logpiso[:,jj])
         #Find bin to which these contribute
-        thetabin= numpy.floor((theta-plotxgrid[0])/(plotxgrid[1]-plotxgrid[0]))
+        thetabin= numpy.floor((theta-xgrid[0])/(xgrid[1]-xgrid[0])+0.5)
         Rbin= numpy.floor((R-plotygrid[0])/(plotygrid[1]-plotygrid[0]))
         indx= (thetabin < 0)
         thetabin[indx]= 0
         Rbin[indx]= 0
         thisout[indx]= -numpy.finfo(numpy.dtype(numpy.float64)).max
         indx= (thetabin >= 2*res)
-        thetabin[indx]= 0 #Has to be
+        thetabin[indx]= 0. #Has to be
         #Rbin[indx]= 0
         #thisout[indx]= -numpy.finfo(numpy.dtype(numpy.float64)).max
         indx= (Rbin < 0)
@@ -96,34 +98,6 @@ def plot_distanceprior(parser):
         Rbin= Rbin.astype('int')
         for ii in range(len(data)):
             plotthis[thetabin,Rbin,ii]= thisout[ii]
-    """
-    for ii in range(2*res):
-        for jj in range(res):
-            #First calculate d
-            R, theta= ygrid[jj],xgrid[ii]
-            d= math.sqrt(R**2.+1.-2.*R*math.cos(theta))
-            if 1./math.cos(theta) < R and math.cos(theta) > 0.:
-                gridl= math.pi-math.asin(R/d*math.sin(theta))
-            else:
-                gridl= math.asin(R/d*math.sin(theta))
-            dl= 0.2
-            indx= (l > gridl -dl)*(l < gridl + dl)
-            if numpy.sum(indx) == 0.: continue
-            thesedata= data[indx]
-            theseh= h[indx]
-            thesejk= jk[indx]
-            #Calculate isochrone probability
-            logpiso= numpy.zeros(len(thesedata))
-            if d == 0.: d= 0.0000001
-            dm= _dm(d*_REFR0)
-            for kk in range(len(thesedata)):
-                mh= theseh[kk]-dm
-                logpiso[kk]= iso(thesejk[kk],mh)
-            plotthis[ii,jj]= logsumexp(_logpd([0.,1.],d,None,None,
-                                              None,None,None,
-                                              options,R,theta,
-                                              1.,0.,logpiso))
-    """                                         
     #Normalize
     for ii in range(2*res):
         for jj in range(res):
@@ -131,11 +105,55 @@ def plot_distanceprior(parser):
     plotthis= plotthis[:,:,0]
     plotthis-= numpy.amax(plotthis)
     plotthis= numpy.exp(plotthis)
+    plotthis[(plotthis == 0.)]= numpy.nan
+    #Get los
+    locations= list(set(data['LOCATION']))
+    nlocs= len(locations)
+    l_plate= numpy.zeros(nlocs)
+    for ii in range(nlocs):
+        indx= (data['LOCATION'] == locations[ii])
+        l_plate[ii]= numpy.mean(data['GLON'][indx])
     bovy_plot.bovy_print()
     ax= pyplot.subplot(111,projection='galpolar')#galpolar is in bovy_plot
     vmin, vmax= 0., 1.
     out= ax.pcolor(plotxgrid,plotygrid,plotthis.T,cmap='gist_yarg',
-                   vmin=vmin,vmax=vmax)
+                   vmin=vmin,vmax=vmax,zorder=2)
+    #Overlay los
+    for ii in range(nlocs):
+        lds= numpy.linspace(0.,2.95,501)
+        lt= numpy.zeros(len(lds))
+        lr= numpy.zeros(len(lds))
+        lr= numpy.sqrt(1.+lds**2.-2.*lds*numpy.cos(l_plate[ii]*_DEGTORAD))
+        lt= numpy.arcsin(lds/lr*numpy.sin(l_plate[ii]*_DEGTORAD))
+        indx= (1./numpy.cos(l_plate[ii]*_DEGTORAD) < lds)*(numpy.cos(l_plate[ii]*_DEGTORAD) > 0.)
+        lt[indx]= numpy.pi-lt[indx]
+        ax.plot(lt,lr,
+               ls='-',color='w',zorder=3)      
+    from matplotlib.patches import Arrow, FancyArrowPatch
+    arr= FancyArrowPatch(posA=(-math.pi/2.,1.8),
+                         posB=(-math.pi/4.,1.8),
+                         arrowstyle='->', 
+                         connectionstyle='arc3,rad=%4.2f' % (-math.pi/16.),
+                         shrinkA=2.0, shrinkB=2.0, mutation_scale=20.0, 
+                         mutation_aspect=None,fc='k')
+    ax.add_patch(arr)
+    bovy_plot.bovy_text(-math.pi/2.,1.97,r'$\mathrm{Galactic\ rotation}$',
+                         rotation=-22.5)
+    radii= numpy.array([0.5,1.,1.5,2.,2.5])
+    labels= []
+    for r in radii:
+        ax.plot(numpy.linspace(0.,2.*math.pi,501,),
+                numpy.zeros(501)+r,ls='-',color='0.65',zorder=1,lw=0.5)
+        labels.append(r'$%i$' % int(r*8.))
+    pyplot.rgrids(radii,labels=labels,angle=-32.5)
+    bovy_plot.bovy_text(5.785,2.82,r'$\mathrm{kpc}$')
+    azs= numpy.array([0.,45.,90.,135.,180.,225.,270.,315.])*_DEGTORAD
+    for az in azs:
+        ax.plot(numpy.zeros(501)+az,
+                numpy.linspace(0.,2.8,501),'-',color='0.6',lw=0.5,zorder=1)
+    #Sun
+    bovy_plot.bovy_text(0.065,.9075,r'$\odot$')
+    pyplot.ylim(0.,2.8)
     bovy_plot.bovy_end_print(options.plotfile)
     
 
