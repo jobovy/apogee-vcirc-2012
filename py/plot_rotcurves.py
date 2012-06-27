76e6ec9ba54690c95f429679973d823465fbb0b3
Jo Bovy <jo.bovy@gmail.com>
2012-06-27 18:28:21 +0000

    vclos: add more fake fits

--- py/plot_rotcurves.py
+++ py/plot_rotcurves.py
@@ -6,21 +6,21 @@ _PLOTM31= True
 def plot_rotcurves(plotfilename):
     rs= numpy.linspace(3.5,16.,1001)
     #1: flat
-    vo,ro= 0.93042695, 1.00408592
+    vo,ro= 0.92772766, 1.00939298
     vflat= rs**0.*vo*_REFV0
     #2: power-law
-    vo, ro, beta= 0.92351186, 1.00082572, -0.08535421
+    vo, ro, beta= 0.92881531,  1.0009845, 0.00750639
     vpl= vo*(rs/_REFR0/ro)**beta*_REFV0
     #3: linear
-    vo, ro, dvdr= 0.92640056, 1.00226682, -0.05341841
+    vo, ro, dvdr= 0.93050113,  1.00377579, 0.01625223
     vlinear= (vo+dvdr*(rs/_REFR0-1.))*_REFV0
     #4: quadratic
-    vo, ro, dvdr, d2vdr2= 0.93287216, 1.00646025, -0.09445865, 0.04764972
+    vo, ro, dvdr, d2vdr2= 0.92954738,  1.00440273, 0.02909439,  0.2#5129582
     vquadratic= (vo+dvdr*(rs/_REFR0-1.)+d2vdr2*(rs/_REFR0-1.)**2.)*_REFV0
     #5: cubic
-    #vo, ro, dvdr, d2vdr2, d3vdr3= 1.02757215,  1.07745537, -0.41021252, -0.4103778,   1.34092465
-    #vcubic= (vo+dvdr*(rs/_REFR0-1.)+d2vdr2*(rs/_REFR0-1.)**2.\
-    #             +d3vdr3*(rs/_REFR0-1.)**3.)*_REFV0
+    vo, ro, dvdr, d2vdr2, d3vdr3=  0.9416574, 1.01691786, -0.10708624, -0.07733835,  0.42241186
+    vcubic= (vo+dvdr*(rs/_REFR0-1.)+d2vdr2*(rs/_REFR0-1.)**2.\
+                 +d3vdr3*(rs/_REFR0-1.)**3.)*_REFV0
     #Plot all
     bovy_plot.bovy_print(fig_width=8.)
     bovy_plot.bovy_plot(rs,vflat,'k-',
@@ -31,7 +31,7 @@ def plot_rotcurves(plotfilename):
     bovy_plot.bovy_plot(rs,vpl,'k--',overplot=True)
     bovy_plot.bovy_plot(rs,vlinear,'k-.',overplot=True)
     bovy_plot.bovy_plot(rs,vquadratic,'k:',overplot=True)
-    #bovy_plot.bovy_plot(rs,vcubic,'k-.',overplot=True,color='red')
+    bovy_plot.bovy_plot(rs,vcubic,'k-.',overplot=True,color='red')
     if _PLOTM31:
         #Read file
         m31data= numpy.loadtxt('../data/m31.dat',comments='#')
