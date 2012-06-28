6eba64823d33b4b665f1dbe621318c940865c8ba
Jo Bovy <jo.bovy@gmail.com>
2012-06-28 22:33:30 +0000

    vclos: plots of rotation curve for mock data

--- py/plot_rotcurve_sigmas.py
+++ py/plot_rotcurve_sigmas.py
@@ -1,21 +1,43 @@
 import os, os.path
 import cPickle as pickle
 from optparse import OptionParser
+import math
 import numpy
 from scipy import special
 from fitvc import get_options, _DEGTORAD, _REFR0, _REFV0, _VRSUN, _PMSGRA
 from galpy.util import bovy_plot
+from matplotlib import pyplot
 from plot_pdfs import set_options, load_samples
-_PLOTM31= False
 def plot_rotcurve_samples(options=None,args=None):
     """Plot sample rotation curves"""
     options= set_options(options)
     params= numpy.random.permutation(load_samples(args[0]))
+    if options.removerooutliers:
+        indx= numpy.zeros(len(params),dtype='bool')
+        indx[:]= True
+        for ii in range(len(params)):
+            if params[ii][1] > (9./_REFR0): indx[ii]= False
+        params= params[indx]
+        options.nsamples= numpy.amin([options.nsamples,len(params)])
+        print len(params)
+    bestfitparams= load_samples(args[1])
     rotcurves= []
-    rs= numpy.linspace(3.5,16.,1001)
+    rs= numpy.linspace(3.5,14.,1001)
+    fs= numpy.zeros((len(rs),options.nsamples))
+    besfitrc= numpy.zeros_like(rs)
     add= 0
     if options.nooutliermean: add-= 1
     if not options.dwarf: add-= 1
+    if options.rotcurve.lower() == 'flat':
+        bestfitrc= bestfitparams[0]*rs**0.*_REFV0
+    elif options.rotcurve.lower() == 'powerlaw':
+        bestfitrc= bestfitparams[0]*(rs/_REFR0/bestfitparams[1])**bestfitparams[6+add]*_REFV0
+    elif options.rotcurve.lower() == 'linear':
+        bestfitrc= (bestfitparams[0]+bestfitparams[6+add]*(rs/_REFR0/bestfitparams[1]-1.))*_REFV0
+    elif options.rotcurve.lower() == 'quadratic':
+        bestfitrc= (bestfitparams[0]+bestfitparams[6+add]*(rs/_REFR0/bestfitparams[1]-1.)+bestfitparams[7+add]*(rs/_REFR0/bestfitparams[1]-1)**2.)*_REFV0
+    elif options.rotcurve.lower() == 'cubic':
+        bestfitrc= (bestfitparams[0]+bestfitparams[6+add]*(rs/_REFR0/bestfitparams[1]-1)+bestfitparams[7+add]*(rs/_REFR0/bestfitparams[1]-1.)**2.+bestfitparams[8+add]*(rs/_REFR0/bestfitparams[1]-1.)**3.)*_REFV0
     for ii in range(options.nsamples):
         if options.rotcurve.lower() == 'flat':
             thisrc= params[ii][0]*rs**0.*_REFV0
@@ -27,17 +49,38 @@ def plot_rotcurve_samples(options=None,args=None):
             thisrc= (params[ii][0]+params[ii][6+add]*(rs/_REFR0/params[ii][1]-1.)+params[ii][7+add]*(rs/_REFR0/params[ii][1]-1)**2.)*_REFV0
         elif options.rotcurve.lower() == 'cubic':
             thisrc= (params[ii][0]+params[ii][6+add]*(rs/_REFR0/params[ii][1]-1)+params[ii][7+add]*(rs/_REFR0/params[ii][1]-1.)**2.+params[ii][8+add]*(rs/_REFR0/params[ii][1]-1.)**3.)*_REFV0
-        rotcurves.append(thisrc)
+        fs[:,ii]= thisrc
+    #Now plot the mean and std-dev from the posterior
+    rcmean= numpy.zeros(len(rs))
+    nsigs= 3
+    rcsigs= numpy.zeros((len(rs),2*nsigs))
+    #Record mean and std-devs
+    rcmean[:]= numpy.mean(fs,axis=1)
     bovy_plot.bovy_print(fig_width=8.)
-    ii= 0
-    bovy_plot.bovy_plot(rs,rotcurves[ii],'-',color='0.65',
+    bovy_plot.bovy_plot(rs,bestfitrc,'k-',
                         xlabel=r'$R\ [\mathrm{kpc}]$',
                         ylabel=r'$V_c\ [\mathrm{km\ s}^{-1}]$',
                         xrange=[0.,20.],
                         yrange=[150.,300.])
-    for ii in range(1,options.nsamples):
-        bovy_plot.bovy_plot(rs,rotcurves[ii],'-',color='0.65',overplot=True,alpha=0.1)
-    if _PLOTM31:
+    for ii in range(nsigs):
+        for jj in range(len(rs)):
+            thisf= sorted(fs[jj,:])
+            thiscut= 0.5*special.erfc((ii+1.)/math.sqrt(2.))
+            rcsigs[jj,2*ii]= thisf[int(math.floor(thiscut*options.nsamples))]
+            thiscut= 1.-thiscut
+            rcsigs[jj,2*ii+1]= thisf[int(math.floor(thiscut*options.nsamples))]
+    colord, cc= (1.-0.75)/(nsigs+1.), 1
+    nsigma= nsigs
+    pyplot.fill_between(rs,rcsigs[:,0],rcsigs[:,1],color='0.75')
+    while nsigma > 1:
+        pyplot.fill_between(rs,rcsigs[:,cc+1],rcsigs[:,cc-1],
+                            color='%f' % (.75+colord*cc))
+        pyplot.fill_between(rs,rcsigs[:,cc],rcsigs[:,cc+2],
+                            color='%f' % (.75+colord*cc))
+        cc+= 2
+        nsigma-= 1
+    bovy_plot.bovy_plot(rs,bestfitrc,'k-',overplot=True)
+    if options.plotm31:
         #Read file
         m31data= numpy.loadtxt('../data/m31.dat',comments='#')
         rm31= m31data[:,0]
@@ -47,6 +90,29 @@ def plot_rotcurve_samples(options=None,args=None):
         indx= (rm31 > 15.2)*(rm31 <= 16.8)
         bovy_plot.bovy_plot([17.,rm31[indx]],[260.,vcm31[indx]],'k-',
                             overplot=True)
+    #Labels
+    if options.rotcurve.lower() == 'flat':
+        print "This is silly"
+    elif options.rotcurve.lower() == 'powerlaw':
+        bovy_plot.bovy_text(r'$\mathrm{Power\!\!-\!\!law\ rotation\ curve\ model}$',
+                            top_left=True,size=16.)
+    elif options.rotcurve.lower() == 'linear':
+        bovy_plot.bovy_text(r'$\mathrm{Linearly rising\ rotation\ curve\ model}$',
+                            top_left=True,size=16.)
+    elif options.rotcurve.lower() == 'quadratic':
+        bovy_plot.bovy_text(r'$\mathrm{Quadratic polynomial\ rotation\ curve\ model}$',
+                            top_left=True,size=16.)
+    elif options.rotcurve.lower() == 'cubic':
+        bovy_plot.bovy_text(r'$\mathrm{Cubic\ polynomial\ rotation\ curve\ model}$',
+                            top_left=True,size=16.)
+    if options.removerooutliers:
+        bovy_plot.bovy_text(r'$R_0 < 9\,\mathrm{kpc}$',
+                            top_right=True,size=16.)
+        
+    if options.addmock2:
+        bovy_plot.bovy_text(r'$\mathrm{Mock\ 2}$',
+                            bottom_right=True,size=16.)
+        
     bovy_plot.bovy_end_print(options.plotfilename)
     return None
 
@@ -121,6 +187,18 @@ def get_options():
                       help="setting this adds dwarf contamination")
     parser.add_option("--nsamples",dest='nsamples',default=10,type='int',
                       help="Number ofsamples to plot")
+    parser.add_option("--removerooutliers",action="store_true", 
+                      dest="removerooutliers",
+                      default=False,
+                      help="setting this removes samples with R0 > 9 kpc")
+    parser.add_option("--plotm31",action="store_true", 
+                      dest="plotm31",
+                      default=False,
+                      help="setting this plots the rotation curve for M31")
+    parser.add_option("--addmock2",action="store_true", 
+                      dest="addmock2",
+                      default=False,
+                      help="setting this will add a label that this is a mock (2)")
     #Output file
     parser.add_option("-o",dest='plotfilename',default=None,
                       help="Name of the file for the plot")
